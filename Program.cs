decd3f454e862b42e7f77d0223d9044bec891b0c
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-01-26 17:10:17 -0500

    Added Extension methods

--- Program.cs
+++ Program.cs
@@ -2,21 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.UseContactsApi();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1",
-    new() { Title="Fritz's Contacts API", Version="v1"});
-});
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapContactsApi();
-
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint(
-    "/swagger/v1/swagger.json",
-    "v1"
-));
 
 app.Run();
